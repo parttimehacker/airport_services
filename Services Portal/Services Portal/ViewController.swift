7b45eaccc9d3a4a363bd7b0db28302083778ba65
David WIlson <parttimehacker@gmail.com>
2022-03-30 08:41:04 -0700

    First Commit

---
+++ Services Portal/Services Portal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Services Portal
+//
+//  Created by David Wilson on 3/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
